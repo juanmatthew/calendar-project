80260cb76c112a8469ee5092abb91ab27669a04a
juanmatthew <101286477+JuaniMatthew@users.noreply.github.com>
2022-11-18 18:32:24 -0500

    Added code to build the days onto the calendar without needing it in HTML and started code for chevron icons

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 
 
 const currentDate = document.querySelector(".current-date");
+const daysTag = document.querySelector(".days");
+const prevNextIcon = document.querySelectorAll(".icons span");
 
 
 //getting the new date, current year and month
@@ -10,16 +12,32 @@ currMonth = date.getMonth();
 
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
+//Building the Calendar days to appear without being in HTML
 const renderCalendar = () => {
     //getting the last date of the month
     let lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate();
+    let liTag = "";
 
     for (let i = 1; i <= lastDateofMonth; i++) {
-        console.log(i);
+        // console.log(i);
+        //so we don't need to add each individual li tag for days
+        liTag += `<li>${i}</li>`;
     }
 
     //title month and year
     currentDate.innerText = `${months[currMonth]} ${currYear}`;
+    daysTag.innerHTML = liTag;
 }
 
 renderCalendar();
+
+//Icons
+prevNextIcon.forEach(icon => {
+    //adding a click event on both chevron icons
+    icon.addEventListener("click", () => {
+        // console.log(icon);
+
+        //if the prev icon is click then it minuses the month by 1, else it adds by 1
+        currMonth = icon.id === "prev" ? currMonth - 1 : currMonth + 1;
+    });
+});
