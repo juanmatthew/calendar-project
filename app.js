6716ddabddd14c35a07e598d63d2214f90cbd2d3
juanmatthew <101286477+JuaniMatthew@users.noreply.github.com>
2022-11-25 14:22:13 -0500

    Updated icon event listener to incorporate next year's dates as well as the previous months

--- app.js
+++ app.js
@@ -27,8 +27,11 @@ const renderCalendar = () => {
 
     for (let i = 1; i <= lastDateofMonth; i++) { // all of the days in the current month
         // console.log(i);
+        //adding the active class if the current date, month, year matches
+        let isToday = i === date.getDate() && currMonth === new Date().getMonth()
+                     && currYear === new Date().getFullYear() ? "active" : ""; 
         //so we don't need to add each individual li tag for days
-        liTag += `<li>${i}</li>`;
+        liTag += `<li class = "${isToday}">${i}</li>`;
     }
 
     for (let i = firstDateofNextMonth; i < 6; i++) { //first days of the next month
@@ -50,6 +53,17 @@ prevNextIcon.forEach(icon => {
 
         //if the prev icon is click then it minuses the month by 1, else it adds by 1
         currMonth = icon.id === "prev" ? currMonth - 1 : currMonth + 1;
+
+        //to inccorporate next year's dates
+        if (currMonth < 0 || currMonth > 11) { //if current month is less than 0 or greater than 11
+
+            //creating a new date of current year and month and passing it as a date value
+            date = new Date(currYear, currMonth);
+            currYear = date.getFullYear(); //updating current year with new date year
+            currMonth = date.getMonth(); //updating current month with new date month
+        } else { //else pass new date as date value
+            date = new Date();
+        }
 
         //calling the calendar
         renderCalendar();
